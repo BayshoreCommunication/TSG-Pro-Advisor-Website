ced59d0c5087037c51b3fe23b253ae4513ba6850
sajolbd <shamim15-9760@diu.edu.bd>
2025-12-07 16:39:39 +0600

    stats section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // import Blog from "@/components/shared/Blog";
 import Hero from "@/components/homepage/Hero";
+import StatsSection from "@/components/homepage/StatsSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <StatsSection/>
     </>
   );
 }
