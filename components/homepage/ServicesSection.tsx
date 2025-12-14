ca5307e143b926b1bb6cd143ff442b45f12ca3bd
sajolbd <shamim15-9760@diu.edu.bd>
2025-12-14 12:58:16 +0600

    secvice section fix

--- components/homepage/ServicesSection.tsx
+++ components/homepage/ServicesSection.tsx
@@ -33,7 +33,7 @@ export default function ServicesSection() {
           </Reveal>
         </div>
 
-        <div className="grid md:grid-cols-3 gap-64 text-center md:text-left ">
+        <div className="grid md:grid-cols-3 md:gap-64 gap-10 text-center md:text-left ">
 
            <Reveal x={100} opacityFrom={0} duration={3}>
           <div className="md:mt-16 mt-0 max-w-[300px]">
